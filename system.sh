56de73d33437d0d083146987964405b79c1803e1
Christian Salway <cs@fxc.im>
2016-04-03 14:19:03 +0100

    inital

---
+++ system.sh
@@ -0,0 +1,12 @@
+cat >> /etc/modules <<EOF
+nf_conntrack
+EOF
+
+cat > /etc/sysctl.d/99-local.conf <<EOF
+net.ipv4.ip_local_port_range = 1024 65535
+net.nf_conntrack_max = 150000
+net.core.somaxconn = 100000
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
+EOF
