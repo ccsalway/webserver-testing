f9a5230cc24a68a101c0e38e31546b7349dd052c
Christian Salway <ccsalway@yahoo.co.uk>
2016-07-20 13:36:20 +0100

    Update system.sh

--- system.sh
+++ system.sh
@@ -5,8 +5,9 @@ EOF
 cat > /etc/sysctl.d/99-local.conf <<EOF
 net.ipv4.ip_local_port_range = 1024 65535
 net.nf_conntrack_max = 150000
-net.core.somaxconn = 100000
+net.core.somaxconn = 10000
 net.ipv4.tcp_fin_timeout = 10
 net.ipv4.tcp_tw_recycle = 1
 net.ipv4.tcp_tw_reuse = 1
+fs.file-max = 100000
 EOF
